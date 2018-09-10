f045bb76d5ab27ca3399fb83487366e22908db38
bvanbeynum <maildrop444@gmail.com>
2018-09-10 16:53:45 +0000

    initialize

---
+++ server.js
@@ -0,0 +1,18 @@
+// Setup =======================================================================
+
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8080;
+
+
+// Config =======================================================================
+
+app.set("x-powered-by", false);
+app.set("root", __dirname);
+
+// Routes =======================================================================
+
+// listen (start app with node server.js) ======================================
+
+app.listen(port);
+console.log("App listening on port " + port);
