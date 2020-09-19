1e459b0d4a0f5bc1fb693200b2c96c75d93e7252
Brett van Beynum <maildrop444@gmail.com>
2020-09-19 19:38:41 -0400

    Increased file upload limit

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ else {
 app.set("x-powered-by", false);
 app.set("root", __dirname);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(busboy()); 
 
