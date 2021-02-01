0f1e9d8894874a9335c95c58d844ffc16577d5d0
Brett van Beynum <maildrop444@gmail.com>
2021-02-01 18:08:42 -0500

    File size limit testing

--- server.js
+++ server.js
@@ -25,8 +25,10 @@ app.set("x-powered-by", false);
 app.set("root", __dirname);
 
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(busboy()); 
 
 // Routes =======================================================================
